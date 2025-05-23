f532ff4cd9827778de37fe691288509e07e22307
MaheshK0901 <maheshkumara0901@gmail.com>
2025-05-23 15:11:37 +0530

    Add main components

---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Balance() {
+  return (
+    <div className='container'>
+      <h4>Your Balance</h4>
+      <h1>Rs. 0.00</h1>
+    </div>
+  )
+}
+
